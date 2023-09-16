0af97df11306aadb7ab06ec98ec48cc07b8d862e
Simon Chareyron <schareyron@ippon.fr>
2023-09-16 18:01:05 +0200

    Remove rotate use case

--- src/main/java/com/ippon/kata/tetris/executing/application/usecase/RotateTetrominoUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.ippon.kata.tetris.executing.application.usecase;
-
-import com.ippon.kata.tetris.executing.application.domain.BoardId;
-import com.ippon.kata.tetris.executing.application.domain.TetrominoMovedEvent;
-
-public interface RotateTetrominoUseCase {
-
-    TetrominoMovedEvent rotate(BoardId boardId);
-}
