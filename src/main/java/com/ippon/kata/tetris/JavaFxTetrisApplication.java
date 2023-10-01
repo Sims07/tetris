aab06ef7c7c18e52fd2b68b299fae74cf0a66ab5
Simon Chareyron <schareyron@ippon.fr>
2023-10-01 14:31:26 +0200

    :improvement: use virtual thread

--- src/main/java/com/ippon/kata/tetris/JavaFxTetrisApplication.java
+++ src/main/java/com/ippon/kata/tetris/JavaFxTetrisApplication.java
@@ -2,30 +2,20 @@ package com.ippon.kata.tetris;
 
 import com.ippon.kata.tetris.shared.primary.javafx.TetrominoGame;
 import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.task.support.TaskExecutorAdapter;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 @EnableAsync
 public class JavaFxTetrisApplication implements AsyncConfigurer {
 
-  public static final int CORE_POOL_SIZE = 20;
-  public static final int MAX_POOL_SIZE = 100;
-  public static final int QUEUE_CAPACITY = 500;
-  public static final String THREAD_NAME_PREFIX = "tetris-";
-
   @Override
   public Executor getAsyncExecutor() {
-    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-    executor.setCorePoolSize(CORE_POOL_SIZE);
-    executor.setMaxPoolSize(MAX_POOL_SIZE);
-    executor.setQueueCapacity(QUEUE_CAPACITY);
-    executor.setThreadNamePrefix(THREAD_NAME_PREFIX);
-    executor.initialize();
-    return executor;
+    return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
   }
 
   public static void main(String[] args) {
